50527bdc2150b6abc80a0d1a66fe0ed352c2f633
Diện Vô <dung251997@gmail.com>
2018-12-12 19:22:25 +0700

    fix footer

--- global/template-components/footer.php
+++ global/template-components/footer.php
@@ -5,20 +5,10 @@
         <div class="flexBoxFooter">
             <div class="footerCS">
                 <div class="collumnFooterC5">
-<!--
-                    <a href="#">
-                        <div class="gd-logo-white"></div>
-                    </a>
--->
                     <div class="titleSection linkFooter">Globe Dreamers
                         <div class="gd-footer-arrow-bottom"></div>
                     </div>
                     <div class="textUnderLogoFooter">
-                        <!--
-                              <strong>Notre promesse</strong>
-                              <p>Créer l'opportunité de voyage pour impacter positivement le monde. </p>
-                            </div>
-                    -->
                         <div class="titleSection linkFooter">
                             <div class="gd-footer-arrow-bottom"></div>
                         </div>
@@ -26,29 +16,10 @@
                             <li><a href="#">Nos valeurs</a></li>
                             <li><a href="#">Notre équipe</a></li>
                             <li><a href="#">Nous rendre visite</a></li>
-                            <!--          <li><a href="#">Nous contacter</a></li>-->
                             <li><a href="#">Espace presse</a></li>
-<!--                            <li><a href="#">Comment ça marche ? </a></li>-->
-
                         </ul>
                     </div>
                 </div>
-
-                <!--
-                      <div class="collumnFooterC5">
-                        <div class="titleSection linkFooter">Les globedreamers <div class="gd-footer-arrow-bottom"></div></div>
-
-                        <ul class="subMenuFooter">
-                          <li><a href="#">Nos valeurs</a></li>
-                          <li><a href="#">Notre équipe</a></li>
-                          <li><a href="#">Nous rendre visite</a></li>
-                          <li><a href="#">Nous contacter</a></li>
-                          <li><a href="#">Equipe presse</a></li>
-                        </ul>
-
-                      </div>
-                -->
-
                 <div class="collumnFooterC5">
                     <div class="titleSection linkFooter">Financer un projet
                         <div class="gd-footer-arrow-bottom"></div>
@@ -56,33 +27,18 @@
 
                     <ul class="subMenuFooter">
                         <li><a href="#">Financer un projet</a></li>
-<!--                        <li><a href="#">Nos carnets de voyage</a></li>-->
                         <li><a href="#">Crowndfunding</a></li>
                         <li><a href="#">Offre entreprise</a></li>
                     </ul>
 
                 </div>
-
-                <!--
-                      <div class="collumnFooterC5">
-                        <div class="titleSection linkFooter">A propos de la plateforme <div class="gd-footer-arrow-bottom" ></div></div>
-
-                        <ul class="subMenuFooter">
-                          <li><a href="#">Comment ça marche ? </a></li>
-                          <li><a href="#">Nos carnets de voyage</a></li>
-                          <li><a href="#">Questions fréquentes</a></li>
-                        </ul>
-
-                      </div>
-                -->
-
                 <div class="collumnFooterC5">
                     <div class="titleSection linkFooter">Des actus, des conseils
                         <div class="gd-footer-arrow-bottom"></div>
                     </div>
 
                     <ul class="subMenuFooter">
-                       <li><a href="#">Blog Dreamers </a></li>
+                        <li><a href="#">Blog Dreamers </a></li>
                         <li><a href="#">Conseils du gouvernement aux voyageurs</a></li>
                         <li><a href="#">Questions fréquentes</a></li>
                     </ul>
@@ -99,44 +55,19 @@
                         <a class="gridSocialIcons gd-footer-youtube" href="#" style="width: 80px;"></a>
 
                         <a class="gridSocialIcons gd-footer-insta" href="#" style="width: 80px;"></a>
-
                     </div>
                 </div>
 
             </div>
-            <!--
-               <div class="flexBoxFooter verticalAlign">
 
-                  <div class="blankFinancement">
+        </div>
 
-                  </div>
-
-
-
-                  <div class="collumnFooterFinancement flexBoxFooterFinancement">
-                    <div class="gd-mongopay gridFooterFinancementIcon"></div>
-                    <div class="textFooter gridFooterFinancementText">
-                    Globe Dreamers est agent de l’institution financière MANGOPAY SA. Nos paiements sont sécurisés avec MANGOPAY Payment Services. </div>
-                   </div>
-
-               </div>
-            -->
-
-
-
-            <div class="bottomFooter" style="margin: 0 auto; margin-bottom:-40px;">
-               <hr style="width:1920px; margin-top:0px;">
-                <p>GLOBE DREAMERS © Copyright 2018 </p>
-                <a href="#">Mentions légales </a> <a href="#">Conditions générales d'utilisation </a> <a href="#">Confidentialité
-                </a>
-                <a href="#">Politique de cookies </a>
-            </div>
-
-            <!--
-               <div class="financementParticipatifFooter">
-                <div class="gd-financement-participatif"></div><div class="gd-mongopay"></div>
-               </div>
-            -->
+        <div class="bottomFooter" style="margin: 0 auto; padding-bottom: 40px;">
+            <!-- <hr style="width:1920px; margin-top:0px;"> -->
+            <p style="padding-top : 40px;">GLOBE DREAMERS © Copyright 2018 </p>
+            <a href="#">Mentions légales </a> <a href="#">Conditions générales d'utilisation </a> <a href="#">Confidentialité
+            </a>
+            <a href="#">Politique de cookies </a>
         </div>
     </div>
 </footer>
